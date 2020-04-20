2c8ac64105850ef636b0c287738af6772cc3184c
Michael Craig <mcraig@rpa.com>
2020-04-19 21:14:18 -0400

    Add async loading of component code for new post

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,11 +4,16 @@ import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
 import './Blog.css';
 //import axios from 'axios';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+// import NewPost from './NewPost/NewPost';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
     state = {
-        auth: false
+        auth: true
     }
     render () {
 
@@ -37,7 +42,7 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" exact render={() => <h1>Test</h1>} /> */}
                 <Switch>
-                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
                     <Route path="/posts" component={Posts} />
                     <Route render={() => <h1>Not Found</h1>} />
                     {/* <Redirect from="/" to="/posts" /> */}
